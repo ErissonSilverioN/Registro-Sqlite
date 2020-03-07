91811330581ea6970633d5489c30a6c61d33a7e7
Erisson Silverio Nu�ez <erisson_silverio@ucne.edu.do>
2020-03-06 20:56:01 -0400

    Creando Entidad Personas...

---
+++ RegistroConSqlite/Entidades/Personas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegistroConSqlite.Entidades
+{
+    public class Personas
+    {
+        public int PersonaId { get; set; }
+        public string Nombre { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public Personas()
+        {
+            PersonaId = 0;
+            Nombre = string.Empty;
+            Fecha = DateTime.Now;
+        }
+    }
+}
--- RegistroConSqlite/MainWindow.xaml.cs
+++ RegistroConSqlite/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace RegistroConSqlite
         {
             InitializeComponent();
         }
+
+        private void nombreTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
