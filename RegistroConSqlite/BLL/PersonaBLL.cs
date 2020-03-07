aac2f059ad1d56119af6e6a45e5a715d2f8ae605
Erisson Silverio Nu�ez <erisson_silverio@ucne.edu.do>
2020-03-06 21:11:17 -0400

    Creando Clase BLL de Persona...

---
+++ RegistroConSqlite/BLL/PersonaBLL.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroConSqlite.DAL;
+using RegistroConSqlite.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace RegistroConSqlite.BLL
+{
+    public class PersonaBLL
+    {
+        public static bool Guardar(Personas personas)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                if (db.personas.Add(personas) != null)
+                    paso = db.SaveChanges() > 0;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+
+        public static bool Modificar(Personas persona)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                db.Entry(persona).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                paso = (db.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+
+            return paso;
+        }
+
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+            try
+            {
+                var eliminar = db.personas.Find(id);
+                db.Entry(eliminar).State = EntityState.Deleted;
+                paso = (db.SaveChanges() > 0);
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+
+            return paso;
+        }
+
+
+        public static Personas Buscar(int id)
+        {
+            Contexto db = new Contexto();
+            Personas personas = new Personas();
+
+            try
+            {
+                personas = db.personas.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+
+            return personas;
+        }
+
+        public static List<Personas> GetList(Expression<Func<Personas, bool>> persona)
+        {
+
+            List<Personas> Lista = new List<Personas>();
+            Contexto db = new Contexto();
+
+            try
+            {
+                Lista = db.personas.Where(persona).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+
+            return Lista;
+        }
+    }
+}
--- RegistroConSqlite/Entidades/Personas.cs
+++ RegistroConSqlite/Entidades/Personas.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace RegistroConSqlite.Entidades
 {
     public class Personas
     {
+        [Key]
         public int PersonaId { get; set; }
         public string Nombre { get; set; }
         public DateTime Fecha { get; set; }
