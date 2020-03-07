389641afd2ecdee10cc007d61a62af402969c0b7
Erisson Silverio Nu�ez <erisson_silverio@ucne.edu.do>
2020-03-06 21:03:21 -0400

    Creando la Conexion a la base de datos, con Sqlite...

---
+++ RegistroConSqlite/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroConSqlite.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegistroConSqlite.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Personas> personas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite(@"Data Source = Personas.db");
+        }
+    }
+}
